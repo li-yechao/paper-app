5aa8539465875e6fb3e1c384cd8b31de7a9ef98e
LiYechao <liyechao@yechao.xyz>
2022-10-12 13:07:19 +0800

    fix: correct view insets when keyboard visibility changed

--- lib/screens/object/object_screen.dart
+++ lib/screens/object/object_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:paper/state/auth.dart';
+import 'package:paper/widgets/correct_view_insets.dart';
 
 import 'editor.dart' if (dart.library.html) 'editor_web.dart';
 
@@ -24,48 +25,50 @@ class ObjectScreen extends HookConsumerWidget {
     final changed = useState<bool?>(null);
     final loaded = useState(false);
 
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        previousPageTitle: previousPageTitle,
-        middle: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Text.rich(
-            TextSpan(
-              children: [
-                if (changed.value == true) const TextSpan(text: '*'),
-                TextSpan(text: title),
-              ],
+    return CorrectViewInsets(
+      child: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          previousPageTitle: previousPageTitle,
+          middle: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  if (changed.value == true) const TextSpan(text: '*'),
+                  TextSpan(text: title),
+                ],
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.fade,
             ),
-            maxLines: 1,
-            overflow: TextOverflow.fade,
           ),
         ),
-      ),
-      child: SafeArea(
-        bottom: false,
-        child: Stack(
-          children: [
-            if (user != null)
-              Positioned.fill(
-                child: Editor(
-                  userId: user.id,
-                  objectId: objectId,
-                  changed: changed,
-                  loaded: loaded,
+        child: SafeArea(
+          bottom: false,
+          child: Stack(
+            children: [
+              if (user != null)
+                Positioned.fill(
+                  child: Editor(
+                    userId: user.id,
+                    objectId: objectId,
+                    changed: changed,
+                    loaded: loaded,
+                  ),
+                ),
+              Positioned(
+                top: 48.0,
+                left: 0,
+                right: 0,
+                child: Visibility(
+                  visible: user == null || !loaded.value,
+                  child: const Center(
+                    child: CupertinoActivityIndicator(),
+                  ),
                 ),
               ),
-            Positioned(
-              top: 48.0,
-              left: 0,
-              right: 0,
-              child: Visibility(
-                visible: user == null || !loaded.value,
-                child: const Center(
-                  child: CupertinoActivityIndicator(),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/correct_view_insets.dart
@@ -0,0 +1,84 @@
+// Copyright 2022 LiYechao
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'dart:async';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
+
+import 'package:flutter/widgets.dart';
+
+class CorrectViewInsets extends StatefulWidget {
+  const CorrectViewInsets({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  State<CorrectViewInsets> createState() => _CorrectViewInsetsState();
+}
+
+class _CorrectViewInsetsState extends State<CorrectViewInsets> {
+  double _viewInsetsBottom = 0;
+
+  _onResize(Event e) {
+    _calcViewInsets();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    window.visualViewport?.addEventListener('resize', _onResize, true);
+  }
+
+  @override
+  void dispose() {
+    window.visualViewport?.removeEventListener('resize', _onResize, true);
+    super.dispose();
+  }
+
+  Timer? _timer;
+
+  _calcViewInsets() {
+    _timer ??= Timer.periodic(const Duration(milliseconds: 100), (Timer timer) {
+      document.documentElement?.scrollTop = 0;
+      final windowInnerHeight = window.innerHeight!.toDouble();
+      final visualHeight = window.visualViewport!.height!.toDouble();
+
+      setState(() {
+        _viewInsetsBottom = windowInnerHeight - visualHeight;
+      });
+
+      if (timer.tick >= 10) {
+        _timer?.cancel();
+        _timer = null;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+
+    return MediaQuery(
+      data: mediaQuery.copyWith(
+        viewInsets: mediaQuery.viewInsets.copyWith(
+          bottom: _viewInsetsBottom,
+        ),
+      ),
+      child: widget.child,
+    );
+  }
+}
